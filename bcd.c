8a50a804a3b768bc892ce446f5dd576b0ef251c0
tanishhhqq <154842728+tanishhhqq@users.noreply.github.com>
2024-05-08 00:49:50 +0530

    Update bcd.c

--- bcd.c
+++ bcd.c
@@ -1,26 +1,42 @@
-#include <8051.h>
-
-// Function to convert binary to BCD
-unsigned char binaryToBCD(unsigned char binary) {
-    unsigned char tens, units;
-
-    tens = binary / 10;     // Get tens place digit
-    units = binary % 10;    // Get units place digit
-
-    return (tens << 4) | units;  // Combine tens and units into BCD format
-}
+#include <reg51.h>
 
 void main() {
-    unsigned char binaryNum = 25;  // Example binary number
-
-    unsigned char bcdNum = binaryToBCD(binaryNum);  // Convert to BCD
-
-    // Now you can use bcdNum for further processing or output
-
-    // For example, outputting to P1 port (assuming 8051 has 8-bit ports)
-    P1 = bcdNum;
+    unsigned int i; // Use unsigned int for loop variable
 
     while(1) {
-        // Your main program loop
+        for(i = 0; i < 100; i++) {
+            if(i < 10)
+                P1 = i;
+            else if(i < 20)
+                P1 = i + 6;
+            else if(i < 30)
+                P1 = i + 12;
+            else if(i < 40)
+                P1 = i + 18;
+            else if(i < 50)
+                P1 = i + 24;
+            else if(i < 60)
+                P1 = i + 30;
+            else if(i < 70)
+                P1 = i + 36;
+            else if(i < 80)
+                P1 = i + 42;
+            else if(i < 90)
+                P1 = i + 48;
+            else // i < 100, this else block is redundant
+                P1 = i + 54;
+
+            // Add delay here
+            // You may adjust the delay based on your requirement
+            delay();
+        }
     }
 }
+
+void delay() {
+    unsigned int j, k;
+    for(j = 0; j < 5000; j++) {
+        for(k = 0; k < 20; k++);
+    }
+}
+
