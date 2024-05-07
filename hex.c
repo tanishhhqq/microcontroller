b21c382231acede45ba01efb312187d5168d7054
tanishhhqq <154842728+tanishhhqq@users.noreply.github.com>
2024-05-07 23:30:09 +0530

    Create hex.c

---
+++ hex.c
@@ -0,0 +1,16 @@
+# include <regst.h> 
+#include <stdio.h> 
+void main (){
+  int i,j,k; 
+  while (1)
+    for (i=0; i<16; i++)
+    {
+      PI=1; for(j=0;j<1000;j++ )
+      {
+        for(k=0;k<1000;k++) 
+        {
+          
+        }
+      }
+    }
+}
