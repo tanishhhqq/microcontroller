734b865297d57ddf5602c49939ae998fa450d853
tanishhhqq <154842728+tanishhhqq@users.noreply.github.com>
2024-05-08 05:19:52 +0530

    Create bcdalt.c

---
+++ bcdalt.c
@@ -0,0 +1,87 @@
+#include <reg51.h>
+#include <stdio.h>
+void main()
+{
+	
+	
+	long int j,k;
+	int i;
+	while(1){
+
+ 		for(i=0;i<100;i++){
+			if(i<10){
+				P1=i;
+				for(j=0;j<10000;j++){
+					for(k=0;k<20;k++);
+				
+
+		  }
+			}
+			
+			else if(i<20) {
+				P1=i+6;
+				for(j=0;j<10000;j++){
+					for(k=0;k<20;k++);
+				
+		  }}
+		
+				else if(i<30) {
+					P1=i+12;
+				for(j=0;j<5000;j++){
+					for(k=0;k<20;k++);
+				
+		  }
+		}
+			else if(i<40) {
+				P1=i+18;
+				for(j=0;j<5000;j++){
+					for(k=0;k<20;k++);
+				
+		  }
+		}
+			else if(i<50) {
+				P1=i+24;
+				for(j=0;j<5000;j++){
+					for(k=0;k<20;k++);
+				
+		  }
+		}
+			else if(i<60) {
+				P1=i+30;
+				for(j=0;j<5000;j++){
+					for(k=0;k<20;k++);
+				
+		  }
+		}
+			else if(i<70) {
+				P1=i+36;
+				for(j=0;j<5000;j++){
+					for(k=0;k<20;k++);
+				
+		  }
+		}
+			else if(i<80) {
+				P1=i+42;
+				for(j=0;j<5000;j++){
+					for(k=0;k<20;k++);
+				
+		  }
+		}
+			else if(i<90) {
+				P1=i+48;
+				for(j=0;j<5000;j++){
+					for(k=0;k<20;k++);
+				
+		  }
+		}
+			else if(i<100) {
+				P1=i+54;
+				for(j=0;j<5000;j++){
+					for(k=0;k<20;k++);
+				
+		  }
+		}
+	}
+			
+}		
+}
